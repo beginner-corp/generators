c9a5fcc88aa87d90705ab9f367165de8b46b6bc6
Paul Chin Jr <pchinjr@gmail.com>
2021-04-29 14:08:28 -0400

    pauls branch

--- src/static.js
+++ src/static.js
@@ -1,3 +1,17 @@
+const fs = require('fs');
+
 module.exports = function static(props={}) {
-  return true
+  fs.readFile('./public/index.html', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err)
+      return
+    }
+    fs.mkdirSync('./public_copy')
+    fs.writeFile('./public_copy/index.html', data, err => {
+      if (err) {
+        console.error(err)
+        return
+      }
+    })
+  })
 }
--- test/static.test.js
+++ test/static.test.js
@@ -1,8 +1,15 @@
 const test = require('tape')
 const staticGenerator = require('../src/static')
+const fs = require('fs')
 
 test('Static generator exists', t => {
   t.plan(1)
   t.ok(staticGenerator, 'Static generator exists')
   t.end()
 })
+
+test('Index.html was created', t => {
+  t.plan(1)
+  t.ok(staticGenerator)
+  t.end()
+} )
