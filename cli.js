192ea083505091657b136df4760c4fd3ec965e70
Paul Chin Jr <pchinjr@gmail.com>
2021-05-25 11:15:41 -0400

    adds static creation

--- cli.js
+++ cli.js
@@ -3,6 +3,7 @@ const path = require('path')
 const mkdirp = require('mkdirp')
 const auth = require('./src/auth')
 const mvc = require('./src/mvc')
+const static = require('./src/static')
 
 // const tmp = path.join(process.cwd(), 'tmp')
 
@@ -11,6 +12,16 @@ const argv = require('yargs/yargs')(process.argv.slice(2))
   .help('help').alias('help', 'h')
   .command('mvc', 'generates mvc example')
   .command('auth', 'generates auth example')
+  .command('static', 'generates static example')
+  .options({
+    'd': {
+      alias: 'dest',
+      demandOption: true,
+      default: 'tmp',
+      describe: 'destination path',
+      type: 'string'
+    }
+  })
   .showHelpOnFail(false, 'whoops, something went wrong! run with --help')
   .argv
 
@@ -29,9 +40,11 @@ async function cli () {
     let tmp = path.join(process.cwd(), argv.dest)
     await mkdirp(tmp)
     await auth({ dest: tmp })
+    console.log('created auth example')
   }
 
   if (argv._[0] === 'mvc') {
+    let tmp = path.join(process.cwd(), argv.dest)
     await mkdirp(tmp)
     await mvc({
       dest: tmp,
@@ -40,5 +53,14 @@ async function cli () {
       plural: 'posts',
       rest: [ { name: 'title', type: 'string' }, { name: 'content', type: 'text' } ]
     })
+    console.log('created mvc example')
   }
+
+  if (argv._[0] === 'static') {
+    let tmp = path.join(process.cwd(), argv.dest)
+    await mkdirp(tmp)
+    await static({ dest: tmp })
+    console.log('created static example')
+  }
+
 }
