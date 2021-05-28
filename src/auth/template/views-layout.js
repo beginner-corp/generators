a62efad9c947c56e9b162c215e37aa66f11a09fa
Paul Chin Jr <pchinjr@gmail.com>
2021-05-28 09:01:04 -0400

    fixes layout bug in auth example

--- src/auth/template/views-layout.js
+++ src/auth/template/views-layout.js
@@ -3,15 +3,15 @@ module.exports = function () {
 
   let logout = \`<a href =/logout>Logout</a> | <a href=/admin > Admin</a > \`
 
-  let notAuthed = \`<a href = /login>Login</a> | <a href=/register> Register </a > | <a href=/reset > Reset Password</a>\`
+  let notAuthed = \`<a href = /login>Login</a> | <a href=/register> Register </a >\`
 
   return \`
-    < !doctype html >
-  </html >
+  <!doctype html>
+  </html>
     <h1> My Login </h1>
   \${ params.account ? logout : notAuthed }
   \${ params.body }
-  </html >
+  </html>
   \`
 }`
 }
